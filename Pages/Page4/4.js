926ef4c4d688313cb647edb7da8f1eac4fa9bea5
Aprendiz Sistemas <AprendizSistemas@amar.local>
2023-11-30 12:01:20 -0500

    Seguridad Social Agremiadora AMAR

---
+++ Pages/Page4/4.js
@@ -0,0 +1,13 @@
+  //Funciones para avanzar
+
+  const boton = document.getElementById("boton");
+  const avanzarBtn = document.getElementById("avanzarBtn");
+  
+  boton.addEventListener("mouseover", function() {
+      avanzarBtn.disabled = false;
+  });
+  
+  boton.addEventListener("onmouseover", function() {
+      avanzarBtn.disabled = true;
+  });
+  
